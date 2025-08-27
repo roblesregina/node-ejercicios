c5910f3787b0164b40688e1dbe03cfcfcff987e6
roblesregina <regina.robles@anima.edu.uy>
2025-08-27 18:16:32 -0300

    Ejercicio login

--- ejercicio09_login/index.js
+++ ejercicio09_login/index.js
@@ -1,45 +1,32 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const PORT = 8000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/login", verificarCredenciales, (req, res) => {
-  const { username } = req.body;
+const validUser = { username: "pepito", password: "1234" };
 
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="es">
-    <head>
-      <meta charset="UTF-8">
-      <title>Bienvenido</title>
-    </head>
-    <body>
-      <h1>Bienvenido/a ${username}</h1>
-    </body>
-    </html>
-  `);
-});
+function checkCredentials(req, res, next) {
+  const { usuario, contraseña } = req.body;
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
-
-const credenciales = {
-  username: "pepito",
-  password: "1234",
-};
-
-function verificarCredenciales(req, res, next) {
-  const { username, password } = req.body;
-
-  if (
-    username === credenciales.username &&
-    password === credenciales.password
-  ) {
+  if (usuario === validUser.username && contraseña === validUser.password) {
+    req.user = usuario;
     next();
   } else {
     res.send("Credenciales inválidas");
   }
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/login", checkCredentials, (req, res) => {
+  res.send(`<h1>Bienvenido/a ${req.user}</h1>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
